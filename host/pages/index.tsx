c9dd98efa219a1ea1aced9c42bc13e3cc57502f9
Joey Gough <j.gough@novafutur.com>
2022-04-13 17:58:09 +0100

    add host example of routing

--- host/pages/index.tsx
+++ host/pages/index.tsx
@@ -1,22 +1,17 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import dynamic from "next/dynamic";
-import { useState } from 'react';
-// @ts-ignore
-const MfExample = dynamic(() => import('MfExample/MfExample'), {
-  ssr: false,
-  //loading: () => <p>Loading...</p>
-});
+import { useRouter } from 'next/router';
+
 
 const Home: NextPage = () => {
-  const [open, setOpen] = useState<boolean>(false)
+  const { push } = useRouter()
+  const navigateToMfExample = () => {
+    push('/mf-example')
+  }
   return (
     <div className={styles.container}>
       <h2>Module Federation Example</h2>
-      <button onClick={() => setOpen(!open)}>Toggle</button>
-      {open && <MfExample />}
+      <button onClick={() => navigateToMfExample()}>Navigate to Module Federation With Routing Example</button>
     </div>
   )
 }
---
+++ host/pages/mf-example/[[...slug]].tsx
@@ -0,0 +1,57 @@
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+// @ts-ignore
+const MfExample = dynamic(() => import('MfExample/MfExample'), {
+    ssr: false,
+    loading: () => <p>Loading...</p>
+});
+
+const ModuleFederationPage = () => {
+    const { pathname, push, query: { slug } } = useRouter();
+
+    const handleRouteChangeEvent = ({ detail: { route } }: CustomEvent<{ route: string }>) => {
+        push(pathname.replace('[[...slug]]', route));
+    }
+
+    useEffect(() => {
+        window.addEventListener('nova-route-change', handleRouteChangeEvent as EventListener)
+        return () => {
+            window.removeEventListener('nova-route-change', handleRouteChangeEvent as EventListener)
+        }
+    }, []);
+
+    /**
+     * reduce an array of strings to one string seperated by forward slashes
+     */
+    const reducePath = (path: string[]) => path?.reduce((acc, curr) => `${acc}/${curr}`);
+
+    return (
+        <div
+            style={{
+                width: '100vw',
+                height: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+            <h2>
+                dynamic content
+            </h2>
+            <p>Slug: {slug}</p>
+            <div
+                style={{
+                    border: '1px solid #cc0000',
+                    padding: '10px',
+                }}>
+                {/* @ts-ignore */}
+                <MfExample route={reducePath(slug)} />
+            </div>
+        </div>
+    )
+};
+
+export default ModuleFederationPage;
+
