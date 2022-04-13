d099ef15ce6e63f0e1026eb2d1b47e3fa4f0c189
Joey Gough <j.gough@novafutur.com>
2022-04-13 17:59:15 +0100

    remove button

--- mf-example/src/index.tsx
+++ mf-example/src/index.tsx
@@ -1,23 +1,10 @@
 import { Component } from 'react'
 import Router from './Router';
 export class MfExample extends Component<any, any> {
-  /**
-   * function to dispatch custom event
-   */
-  dispatchEvent = () => {
-    const event = new CustomEvent('mf-example-event', {
-      detail: {
-        message: 'Hello from the MfExample component!',
-      },
-    });
-    window.dispatchEvent(event);
-  };
-
   render() {
     return (
       <div>
         <h4>Remote Element</h4>
-        <button type="button" onClick={this.dispatchEvent}>Dispathc Event</button>
         <Router route={this.props.route || ''} />
       </div>
     )
